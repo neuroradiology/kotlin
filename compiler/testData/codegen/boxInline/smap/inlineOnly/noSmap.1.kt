edf6ad629442a75600658b710a53a1edbc984840
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-02-17 15:05:32 +0300

    Don't generate source mapping for 'InlineOnly' functions

---
+++ compiler/testData/codegen/boxInline/smap/inlineOnly/noSmap.1.kt
@@ -0,0 +1,17 @@
+fun box(): String {
+    val z = arrayOf("O", "K")
+    val (x, y) = z
+
+    return x + y
+}
+
+//SMAP
+//noSmap.1.kt
+//Kotlin
+//*S Kotlin
+//*F
+//+ 1 noSmap.1.kt
+//NoSmap_1Kt
+//*L
+//1#1,17:1
+//*E
